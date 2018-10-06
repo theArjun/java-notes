c267e4c78e29a0ac41dcfc95be4bcc0e0c2a8029
thearjun <mailarjunadhikari@gmail.com>
2018-10-06 20:40:34 +0545

    Added compile-time error

--- variables/lifeTImeOfVariable.java
+++ variables/lifeTImeOfVariable.java
@@ -6,6 +6,10 @@ class LifeTime{
             int y=100;  /* Y is initialized every time the loop runs. */
             System.out.println("Value of Y is :"+y);
             y=-1;
+            {
+                /* I created a new scope here to show a compile-time error. */
+                int y;
+            }
             System.out.println("Value of Y now is :"+y);
         }
     }
