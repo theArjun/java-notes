a33c6ce3b9ef122e6719da6e36baebeb850bacaf
thearjun <mailarjunadhikari@gmail.com>
2018-10-06 19:32:32 +0545

    Added src to demonstrate that underscore can be used for readability in larger integer numbers.

---
+++ basics/underScoreForReadability.java
@@ -0,0 +1,9 @@
+class UnderScoreForReadability{
+    public static void main(String args[]){
+        int x;
+
+        x= 123__456__789;
+
+        System.out.print("The value of x : "+x);
+    }
+}
