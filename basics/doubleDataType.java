b31edde6190b9809fe09a9b2f007d4092ba08fcd
thearjun <mailarjunadhikari@gmail.com>
2018-10-06 19:17:40 +0545

    Added src for double data type

---
+++ basics/doubleDataType.java
@@ -0,0 +1,12 @@
+class Double{
+    public static void main(String args[]){
+        double pi,r,a;
+        
+        r=10.8;
+        pi=3.1416;
+
+        a=pi*r*r;
+
+        System.out.println("The area of the circle is: "+a);
+    }
+}
