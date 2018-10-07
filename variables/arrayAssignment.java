bcfc18cab1bac9a344f369c33d98e89d2a12f294
thearjun <mailarjunadhikari@gmail.com>
2018-10-07 07:14:00 +0545

    Added src for assignment of array

---
+++ variables/arrayAssignment.java
@@ -0,0 +1,8 @@
+class ArrayAssignment{
+    public static void main(String args[]){
+        int month[] = {1,2,3,4,5,6,7,8,9,10,11,12};
+        for(int i=0;i<12;i++){
+            System.out.println("Month : "+month[i]);
+        }
+    }
+}
