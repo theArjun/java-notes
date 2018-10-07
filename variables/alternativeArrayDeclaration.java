4da2911efb51619988aa258a5abb1a7f1657eab9
thearjun <mailarjunadhikari@gmail.com>
2018-10-07 09:20:52 +0545

    Added src for alternate way of declaring arrays

---
+++ variables/alternativeArrayDeclaration.java
@@ -0,0 +1,12 @@
+class AlternativeArrayDeclaration{
+    public static void main(String args[]){
+        /* Array can be declared like : type_name[] variable_name; for eg: public static void main(String[] args) */
+        int a1[] = new int[3];
+        int[] a2 = new int[2];
+
+        /* We can also declare multiple arrays at a time by this method. */
+
+        int [] a3,a4,a5  = new int [3];
+        /* which creates the three array variables of integer type  */
+    }
+}
