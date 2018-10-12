2b86b93e42df28658bf36e4c0e8e63a2c45ef06d
Arjun Adhikari <be2017se573@gces.edu.np>
2018-10-12 16:06:15 +0545

    Added src for basic arithmetic operations of integers and doubles

---
+++ operators/basicArithmeticOperators.java
@@ -0,0 +1,33 @@
+class Basic{
+    public static void main(String[] args){
+        
+        // Arithmetic operations using integers
+
+        int a = 1+1;
+        int b = a*3;
+        int c = b/4;
+        int d = c-a;
+        int e = -d;
+
+        System.out.println("a = "+a);
+        System.out.println("b = "+b);
+        System.out.println("c = "+c);
+        System.out.println("d = "+d);
+        System.out.println("e = "+e);
+
+        // Arithmetic operations using doubles
+
+        double da = 1+1;
+        double db = da*3;
+        double dc = db/4;
+        double dd = dc-da;
+        double de = -dd;
+
+        System.out.println("da = "+da);
+        System.out.println("db = "+db);
+        System.out.println("dc = "+dc);
+        System.out.println("dd = "+dd);
+        System.out.println("de = "+de);
+        
+    }
+}
