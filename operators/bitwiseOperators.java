3d70c2662aa89f291ee180de6c52520f6359aec3
Arjun Adhikari <be2017se573@gces.edu.np>
2018-10-14 10:50:43 +0545

    Added src for illustrating bitwise operators

---
+++ operators/bitwiseOperators.java
@@ -0,0 +1,10 @@
+class Bitwise{
+    public static void main(String[] args){
+        int a = 10;
+        int b = 42;
+
+        // Bitwise NOT operator ~
+        System.out.println("Bitwise NOT operator a ~ b = "+~a);
+
+    }
+}
