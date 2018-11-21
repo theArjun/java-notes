842b67b8c815886b7e440712eadf43d7e717722b
Arjun Adhikari <be2017se573@gces.edu.np>
2018-11-21 12:21:17 +0545

    Added src for switch statement.

---
+++ controlStatements/switchStatement.java
@@ -0,0 +1,25 @@
+class SampleSwitch{
+    public static void main(String[] args){
+        for(int i=0;i<6;i++){
+            switch(i){
+                case 0:
+                    System.out.println("i is zero.");
+                    break;
+                case 1:
+                    System.out.println("i is zero.");
+                    break;
+                case 2:
+                    System.out.println("i is zero.");
+                    break;
+                case 3:
+                    System.out.println("i is zero.");
+                    break;
+                case 4:
+                    System.out.println("i is zero.");
+                    break;
+                default:
+                    System.out.println("i is greater than 4.");
+            }
+        }
+    }
+}
