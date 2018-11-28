4a65af163230228e03cbfa975f4535694d8c9f49
Arjun Adhikari <be2017se573@gces.edu.np>
2018-11-28 18:44:42 +0545

    Hello World in Java AWT

---
+++ awt/helloWorld.java
@@ -0,0 +1,11 @@
+import java.awt.*;
+import java.awt.event.*;
+
+class AWTHello {
+    public static void main(String argv[]) {
+      Frame f = new Frame( "Hello world!" );
+      f.addWindowListener( new WindowAdapter(){ public void windowClosing( WindowEvent e ){ System.exit( 0 ); } } );
+      f.setSize( 300, 100 );
+      f.show();
+    }
+}
