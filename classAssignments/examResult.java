113f8ac5d2ca13e9d8aa7f135597bce4552eb0d5
Arjun Adhikari <be2017se573@gces.edu.np>
2018-11-29 07:54:32 +0545

    Added class assignment for Class Percentage

---
+++ classAssignments/examResult.java
@@ -0,0 +1,25 @@
+class Exam{
+    public static void main(String[] args){
+        int percentage = 59;
+
+        if(percentage>100 && percentage<0){
+            if(percentage >= 80){
+                System.out.println("Distinction\n");
+            }
+            else if(percentage >= 60){
+                System.out.println("First Division\n");
+            }
+            else if(percentage >= 50){
+                System.out.println("Second Division\n");
+            }
+            else if(percentage >= 40){
+                System.out.println("Third Division\n");
+            }
+            else{
+                System.out.println("Fail");
+            }
+        }else{
+            System.out.println("Enter a valid percentage.");
+        }
+    }
+}
