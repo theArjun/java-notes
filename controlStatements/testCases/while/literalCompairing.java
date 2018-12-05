7f2c904bd4480b7c3f0a949f1cb5b6bbeb2216c4
Arjun Adhikari <be2017se573@gces.edu.np>
2018-12-06 01:35:36 +0545

    Assigning literal in while statement

---
+++ controlStatements/testCases/while/literalCompairing.java
@@ -0,0 +1,8 @@
+class WhileTest {
+    public static void main(String[] args) {
+        while(10<20){
+            System.out.println("Hello World"); // This statement runs infinitely.
+        }
+        System.out.println("Hello Venus"); // This is not printed and it is not an unreachable statement as it is the process to be checked on compile time.
+    }
+}
