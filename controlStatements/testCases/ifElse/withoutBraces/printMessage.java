7fba6b58bea5b6ec3037a2e8a1407be158b26965
Arjun Adhikari <be2017se573@gces.edu.np>
2018-12-06 00:42:36 +0545

    Added docummentation

--- controlStatements/testCases/ifElse/withoutBraces/printMessage.java
+++ controlStatements/testCases/ifElse/withoutBraces/printMessage.java
@@ -1,6 +1,6 @@
 class IfTest {
     public static void main(String[] args) {
         if(true)    
-            System.out.println("Hello World");
+            System.out.println("Hello World"); // This statement gets executed.
     }
 }
