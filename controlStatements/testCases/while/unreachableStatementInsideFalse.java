bc6b5f8f593632406859642af580459769516e0c
Arjun Adhikari <be2017se573@gces.edu.np>
2018-12-06 01:17:14 +0545

    Corrected Documentation

--- controlStatements/testCases/while/unreachableStatementInsideFalse.java
+++ controlStatements/testCases/while/unreachableStatementInsideFalse.java
@@ -1,8 +1,8 @@
 class WhileTest {
     public static void main(String[] args) {
         while(false){
-            System.out.println("Hello World"); // This statement runs infinitely.
+            System.out.println("Hello World"); // This statement won't run as it's unreachable statement.
         }
-        System.out.println("Hello Venus"); // This statement won't run as it's unreachable statement.
+        System.out.println("Hello Venus"); // 
     }
 }
