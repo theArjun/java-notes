079f0e321bfa12342c2b78e2c77a16705a661d1e
Arjun <be2017se573@gces.edu.np>
2018-12-18 20:18:17 +0545

    Renamed array from x to array

--- array/arrayRepresentation01/arrayRepresentation01.java
+++ array/arrayRepresentation01/arrayRepresentation01.java
@@ -1,19 +1,19 @@
 class ArrayRepresentation{
     public static void main(String[] args){
-        // First the level of the array is pre-defined, and initial index of the array is mentioned in assignation.
-        int[][][] x = new int [2][][]; 
+        // First the level of the array is pre-defined, and initial indearray of the array is mentioned in assignation.
+        int[][][] array = new int [2][][]; 
 
         // First Level
-        x[0] = new int [3][];
-        x[1] = new int [2][];
+        array[0] = new int [3][];
+        array[1] = new int [2][];
 
         // Second Level
-        x[0][0] = new int [1];
-        x[0][1] = new int [2];
-        x[0][2] = new int [3];
+        array[0][0] = new int [1];
+        array[0][1] = new int [2];
+        array[0][2] = new int [3];
 
-        x[1][0] = new int [2];
-        x[1][1] = new int [2];
+        array[1][0] = new int [2];
+        array[1][1] = new int [2];
 
         // In this way, the memory blocks are allocated and no memory leakage happens here.
     }
