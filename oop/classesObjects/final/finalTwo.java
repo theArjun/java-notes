cd21da0386994c12fa05c9440c40fec2ae67e444
Arjun Adhikari <be2017se573@gces.edu.np>
2018-12-24 22:19:37 +0545

    Implements Final Keyword by Constructor

---
+++ oop/classesObjects/final/finalTwo.java
@@ -0,0 +1,13 @@
+public class Sample{
+    final int h;
+    Sample(){
+        h = 8;
+    }
+}
+
+class Final{
+    public static void main(String[] args){
+        Sample sampleObject;
+        System.out.println("H = "+sampleObject.h);
+    }
+}
