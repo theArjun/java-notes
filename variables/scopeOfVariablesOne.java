2d380d4a32de9738288d46c01911edab50598f0e
thearjun <mailarjunadhikari@gmail.com>
2019-01-02 07:53:27 +0545

    Tested another case

--- variables/scopeOfVariablesOne.java
+++ variables/scopeOfVariablesOne.java
@@ -1,9 +1,11 @@
 class Scope{
     public static void main(String[] args){
         if(true){
-            int i = 1;
+            int i ; // For local variable, JVM won't provide default value sp we must provide value explicitly before using it.
+            System.out.println(i); // error: variable i might not have been initialized
         }else{
-            System.out.println(i); //error: cannot find symbol
+            // System.out.println(i); //error: cannot find symbol
+            System.out.println("Hello Eveyone");
         }
     }
 }
