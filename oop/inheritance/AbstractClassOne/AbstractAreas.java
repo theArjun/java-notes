90e58cc866fca7ce55b880a31a5fd2619e25552e
thearjun <mailarjunadhikari@gmail.com>
2019-01-05 17:37:14 +0545

    Renamed variable and corrected syntax

--- oop/inheritance/AbstractClassOne/AbstractAreas.java
+++ oop/inheritance/AbstractClassOne/AbstractAreas.java
@@ -3,12 +3,12 @@ class AbstractAreas{
         Figure figureObjectOne = new Rectangle(10,10); // Illegal now
         Rectangle rectangleObjectOne = new Rectangle(5,9);
 
-        Figure figureObjectReference; // This is OK; no object is created.
-        figureObjectReference = rectangleObjectOne;
-        System.out.println("Area is : "+figureObjectReference.area());
+        Figure figureReference; // This is OK; no object is created.
+        figureReference = rectangleObjectOne;
+        System.out.println("Area is : "+figureReference.area());
 
         Triangle triangleObjectOne = new Triangle(5,4);
-        figureObjectReference = triangleObjectOne;
-        System.out.println("Area is : "+figureObjectReference.area());
+        figureReference = triangleObjectOne;
+        System.out.println("Area is : "+figureReference.area());
     }
 }
