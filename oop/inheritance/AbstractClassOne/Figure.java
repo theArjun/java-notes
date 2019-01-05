d4ac62cc6220cdb5fce03a0ea25208cc75e0f678
thearjun <mailarjunadhikari@gmail.com>
2019-01-05 15:44:10 +0545

    Added Figure Abstract Class

---
+++ oop/inheritance/AbstractClassOne/Figure.java
@@ -0,0 +1,10 @@
+absract class Figure{ // This is abstract class.
+    double dimensionOne;
+    double dimensionTwo;
+    
+    Figure(double dimensionOne, double dimensionTwo){
+        this.dimensionOne = dimensionOne;
+        this.dimensionTwo = dimensionTwo;
+    }
+    abstract double area(); // This is abstract method.
+}
