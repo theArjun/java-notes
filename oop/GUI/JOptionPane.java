bdaf27f039e076a379fa8e06142d58e0387ecd29
thearjun <mailarjunadhikari@gmail.com>
2019-01-05 13:41:12 +0545

    Added documentation

--- oop/GUI/JOptionPane.java
+++ oop/GUI/JOptionPane.java
@@ -1,10 +1,10 @@
-import javax.swing.JOptionPane;
+import javax.swing.JOptionPane; // Importing the GUI Class
 
 class Sum{
     public static void main(String[] args){
-        int firstNo = Integer.parseInt(JOptionPane.showInputDialog("Enter first number :"));
+        int firstNo = Integer.parseInt(JOptionPane.showInputDialog("Enter first number :")); // showInputDialog is method to input the string data, thus it is converted into Integer
         int secondNo = Integer.parseInt(JOptionPane.showInputDialog("Enter second number :"));
         int sum = firstNo + secondNo;
-        JOptionPane.showMessageDialog(null,"The sum is "+sum, "Arjun's Calculator", JOptionPane.PLAIN_MESSAGE);
+        JOptionPane.showMessageDialog(null,"The sum is "+sum, "Arjun's Calculator", JOptionPane.PLAIN_MESSAGE); // Accepts 4 parameters; First -> where to position it, Second -> What to print, Third -> Title Bar, Fourth -> Icon/Message to be placed.
     }
 }
