d8a85283b8c5121198688f6a3e3525a726c4391f
thearjun <mailarjunadhikari@gmail.com>
2019-01-06 17:23:11 +0545

    Added comment

--- exception/Exception.java
+++ exception/Exception.java
@@ -6,7 +6,7 @@ class Exception{
             a = 42/d; // This generates run-time error.
             System.out.println("This will not be printed.");
         }
-        catch(ArithmeticException e){
+        catch(Exception e){ // e is error; also a parameter type of Arithmetic Exception.
             System.out.println("Divide by zero."); // This will get executed.
         }
         System.out.println("After catch.");
