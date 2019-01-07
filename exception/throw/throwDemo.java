afac9112823d8daeed20788cb12a4b397db222df
thearjun <mailarjunadhikari@gmail.com>
2019-01-07 09:03:57 +0545

    Implemets throw

---
+++ exception/throw/throwDemo.java
@@ -0,0 +1,8 @@
+class ThrowDemo{
+    public static void main(String[] args){
+        System.out.println("Example of throw keyword : ");
+        throw new ArithmeticException(); // Yo chahi hamile generate gareko runtime exception
+        // int numberOne  = 2/0; // Yo chahi java runtime environment le generate gareko runtime exception
+        // System.out.println(""+numberOne); 
+    }
+}
