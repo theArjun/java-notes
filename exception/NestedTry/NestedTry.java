82547117b048449653b22aa6f5a9f895c126c1be
thearjun <mailarjunadhikari@gmail.com>
2019-01-07 08:10:52 +0545

    Modified file

--- exception/NestedTry/NestedTry.java
+++ exception/NestedTry/NestedTry.java
@@ -1,18 +1,25 @@
 class NestedTry{
-    public static void main(String[] args){
+    public static void main(String[] args){ // Command Line Arguments
+        System.out.println("Args : "+args[0]);
+        // try{
+        //     int a  = args.length;
+        //     int b = 42 / a;
+        //     System.out.println("A = "+a);
+        //     try{ // Nested Try Block
+        //         if(a==1){
+        //             a = a/(a-a);
+        //         }
+        //     }catch(ArrayIndexOutOfBoundsException e){
+        //         System.out.println("Array index out of bounds : "+e);
+        //     }
+        // }catch(ArithmeticException e){
+        //     System.out.println("Divide by zero."+e);
+        // }
         try{
-            int a  = args.length;
-            int b = 42 / a;
-            System.out.println("A = "+a);
-            try{ // Nested Try Block
-                if(a==1){
-                    a = a/(a-a);
-                }
-            }catch(ArrayIndexOutOfBoundsException e){
-                System.out.println("Array index out of bounds : "+e);
-            }
-        }catch(ArithmeticException e){
-            System.out.println("Divide by zero."+e);
+            args[2] = "Hari";
+            // System.out.println(args[4]);
+        }catch(ArrayIndexOutOfBoundsException error){
+            System.out.println("Error : "+error);
         }
     }
 }
