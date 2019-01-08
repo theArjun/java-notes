bcc217fd066f4e09e7929acb2c03ab92226c6686
thearjun <mailarjunadhikari@gmail.com>
2019-01-08 08:51:53 +0545

    Added steps of Program Flow

--- exception/throws/ThrowsDemo.java
+++ exception/throws/ThrowsDemo.java
@@ -16,3 +16,8 @@ class ThrowsDemo{
         }
     }
 }
+
+// Step 1: Throw new ArithmeticException without handler
+// Step 2: Throw new IllegalAccessException; needs handler
+// Step 3: Throw new IllegalAccessException with try catch
+// Step 4: IllegalAccessException with throws; with handler on main() method
