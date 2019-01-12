fbf4645b1d31ad7a59e9e89fdf44403dd9b71ae1
thearjun <mailarjunadhikari@gmail.com>
2019-01-12 11:19:06 +0545

    Implements Abstract Classes

---
+++ oop/inheritance/AbstractClassThree/Phone.java
@@ -0,0 +1,5 @@
+// Class contains abstract methods, so class is abstract.
+abstract class Phone{
+    // Defining abstract class for showing configuration of various phones.
+    public abstract void showConfig();
+}
