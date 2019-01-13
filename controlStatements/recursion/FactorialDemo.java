53cc04ce62c3ffeb0f66054aa40621a364f81695
thearjun <mailarjunadhikari@gmail.com>
2019-01-13 13:03:55 +0545

    Corrected code; replaced ClassName

--- controlStatements/recursion/FactorialDemo.java
+++ controlStatements/recursion/FactorialDemo.java
@@ -1,6 +1,6 @@
 class FactorialDemo{
     public static void main(String[] args){
-        Fibonacci numberOne = new Fibonacci();
+        Factorial numberOne = new Factorial();
         numberOne.setNumber(5);
         
         System.out.println("Factorial of "+numberOne.getNumber()+" is "+numberOne.getFactNumber());
