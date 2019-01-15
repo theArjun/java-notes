41d7a07d2d5e0d0063e919358d32dc33f33d679d
thearjun <mailarjunadhikari@gmail.com>
2019-01-15 20:11:58 +0545

    Implements throws creating custom exception

---
+++ exception/throws/DemoOne/MyException.java
@@ -0,0 +1,8 @@
+class MyException extends Exception // This inherits the class Exception.
+{
+    MyException(String message)
+    {
+        super(message); // This calls the constructor of class Exception.
+    }
+}
+
