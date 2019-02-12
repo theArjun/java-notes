3f9c6c6312d29de995596add69e0a2a8da15b895
thearjun <mailarjunadhikari@gmail.com>
2019-02-12 16:22:27 +0545

    Demonstrated fillRect

--- graphics/color/withApplet/ColorDemo.java
+++ graphics/color/withApplet/ColorDemo.java
@@ -5,9 +5,12 @@ import java.applet.Applet;
 
 public class ColorDemo extends Applet {
     public void paint(Graphics g) {
-        g.setColor(new Color(255,0,0));
-        g.drawString("Hello World",40,40);
-        g.setColor(new Color(0,255,0));
+        g.setColor(new Color(255, 0, 0));
+        g.fillRect(60, 60, 40, 40);
+        g.drawString("Hello World", 40, 40);
+        g.setColor(new Color(0, 255, 0));
+        g.fillRect(90, 60, 40, 40);
         g.drawString("GCES", 40, 80);
+
     }
 }
