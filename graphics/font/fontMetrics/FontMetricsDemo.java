ef78313a5ccf1f9fa3e68b1533daf693d676a606
thearjun <mailarjunadhikari@gmail.com>
2019-02-12 14:34:01 +0545

    Demonstrates Font Metrics

---
+++ graphics/font/fontMetrics/FontMetricsDemo.java
@@ -0,0 +1,20 @@
+import java.awt.*;
+import java.applet.Applet;
+
+/* <applet code = "FontMetricsDemo" height = "300" width = "300"></applet> */
+
+public class FontMetricsDemo extends Applet {
+    public void paint(Graphics g) {
+        Font myFont = new Font("serif", Font.BOLD + Font.ITALIC, 18);
+        g.setFont(myFont);
+        g.drawString("Hello GCES", 20, 50);
+
+        FontMetrics fm;
+        fm = getFontMetrics(myFont); // Yo chahi graphics context ko properties
+
+        g.drawString("Ascent Value : " + fm.getAscent(), 20, 80);
+        g.drawString("Descent Value : " + fm.getDescent(), 20, 100);
+        g.drawString("Leading Value : " + fm.getLeading(), 20, 120);
+        g.drawString("Height Value : " + fm.getHeight(), 20, 140);
+    }
+}
