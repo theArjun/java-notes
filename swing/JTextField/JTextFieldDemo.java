7eea28b3549ff00b3925368c089fd932c5310fa9
thearjun <mailarjunadhikari@gmail.com>
2019-02-20 13:16:37 +0545

    JTextField Demo
    This question appeared in 2004 Fall

---
+++ swing/JTextField/JTextFieldDemo.java
@@ -0,0 +1,22 @@
+import javax.swing.*;
+import java.awt.*;
+
+public class JTextFieldDemo extends JFrame {
+
+    JTextField jOne;
+
+    JTextFieldDemo(String title) {
+        super(title);
+
+        setSize(400, 400);
+        setVisible(true);
+        setLayout(new FlowLayout());
+
+        jOne = new JTextField("This is default", 20);
+        add(jOne);
+    }
+
+    public static void main(String[] args) {
+        new JTextFieldDemo("JTextField Demo ");
+    }
+}
